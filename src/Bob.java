ccda2e89bc584453f063d9f67f0f93fa7443a2b9
Luis <LuisDEsquivel11@gmail.com>
2023-08-01 18:05:26 -0500

    Bob exercise

---
+++ src/Bob.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Bob {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ask me if I give a...");
+        String input = scanner.nextLine();
+        char inputType = input.charAt(input.length() - 1);
+        String punctuation = String.valueOf(inputType);
+
+        String response = switch(punctuation) {
+            case "?" -> "Sure";
+            case "!" -> "Whoa, chill tf out!";
+            case " " -> "Fine. Be like that, stupid.";
+            default -> "Whatever";
+        };
+
+        System.out.println(response);
+    }
+}
---
+++ src/StringExercise.java
@@ -0,0 +1,19 @@
+public class StringExercise {
+
+    public static void main(String[] args) {
+
+        String pinkFloyd = "We don't need no education \nWe don't need no thought control";
+        System.out.println(pinkFloyd);
+
+        String nestedQuotes = "Check \"this\" out!\", \"s inside of \"s!";
+        System.out.println(nestedQuotes);
+
+        String windows = "In windows, the main drive is usually C:\\";
+        System.out.println(windows);
+
+        String last = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!\n";
+        System.out.println(last);
+
+    }
+}
