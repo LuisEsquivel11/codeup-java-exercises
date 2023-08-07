5be8117718675772e7c86fd970932a069ce6b5ec
Luis <LuisDEsquivel11@gmail.com>
2023-08-07 17:48:53 -0500

    Shapes test completed

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -2,9 +2,20 @@ package shapes;
 
 public class Square extends Rectangle{
 
+
+
+
         public Square(double side) {
-            side = super.length;
-            side = super.width;
+            super.length = side;
+            super.width = side;
+        }
+
+        public double getArea() {
+            return Square.super.length * Square.super.length;
+        }
+
+        public double getPerimeter() {
+            return 4 * Square.super.length;
         }
 
 }
