6f402e16d70d6678b88d9f84868fc851ec139beb
Luis <LuisDEsquivel11@gmail.com>
2023-08-08 20:30:21 -0500

    Grade applications exercise complete

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,11 +1,12 @@
 package grades;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class GradesApplication {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
         HashMap<String, Student> students = new HashMap<>();
         Student luis = new Student("Luis");
         Student cesar = new Student("Cesar");
@@ -28,7 +29,22 @@ public class GradesApplication {
         students.put("SamCanSout", sam);
         students.put("FUImPickleRick", pickleRick);
 
+        System.out.println(students.keySet());
+        String again;
+        do {
+        System.out.println("Which student would you like to learn more about?");
+        String userName = scanner.next();
 
+            if (students.containsKey(userName)) {
+                Object student = students.get(userName);
+                System.out.println("Name: " + ((Student) student).getName() +" Github Username: " + userName + " Grades: " + ((Student) student).getGrades());
+            } else {
+                System.out.println("No users with that username were found.");
+            }
+            System.out.println("Would you like to see another student? y|n");
+             again = scanner.next();
+        } while (again.equalsIgnoreCase("y"));
 
     }
+
 }
